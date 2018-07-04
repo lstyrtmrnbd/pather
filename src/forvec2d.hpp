6fb3d0e2d08efe5d884e10e02f47aadccaf2435b
Marisa Karisame <soymilk@airmail.cc>
2018-07-04 15:42:35 -0400

    Added inverted iterator for drawing y vertically

--- src/forvec2d.hpp
+++ src/forvec2d.hpp
@@ -26,4 +26,23 @@ void forVec2D(std::vector<std::vector<T>>& vec2D,
   }
 }
 
+/**
+ * Iterate through a 2D vector and apply a function,
+ * but with the inner vector as the outer iterator
+ *
+ * Makes the assumption that all inner vectors are the same length as the first!
+ */
+
+template<typename T>
+void forVec2DInner(std::vector<std::vector<T>>& vec2D,
+		   std::function<void(int, int, T&)> elementFunc) {
+
+  for (auto y = 0; y < vec2D.at(0).size(); ++y) {
+
+    for (auto x = 0; x < vec2D.size(); ++x) {
+
+      elementFunc(x, y, vec2D.at(x).at(y));
+    }
+  }
+}
 #endif // FORVEC2D_INCLUDE
